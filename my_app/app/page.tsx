3c73171670a9d46a2ce5d3983873e7f44cf2c365
Akash Bagchi <akashbagchi2710@gmail.com>
2023-01-29 01:14:14 +0530

    spacing and sizing

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -6,13 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-3xl italic underline'>Title 1</h1>
-      <h2 className='text-2xl line-through'>Title 2</h2>
-      <h3 className='text-xl'>Title 3</h3>
-      <p className='text-base overline decoration-red-500'>A regular paragraph</p>
-      <p className='text-sm'>A description paragraph</p>
-      <p className="note text-xs">A little note</p>
-    </div>
+    <main className='flex-column space-y-5'>
+      <div className='bg-red-500 py-5 pl-4 m-2 rounded-lg w-4/5 mx-auto'>
+        <h1 className='text-3xl underline mb-3 font-bold'>Title 1</h1>
+        <h2 className='text-2xl line-through'>Title 2</h2>
+        <h3 className='text-xl'>Title 3</h3>
+      </div>
+      <div className='bg-slate-500 py-5 pl-4 mb-2 rounded-lg w-4/5 mx-auto'>
+        <p className='text-base overline decoration-slate-500'>A regular paragraph</p>
+        <p className='text-sm'>A description paragraph</p>
+        <p className="note text-xs">A little note</p>
+      </div>
+    </main>
   )
 }
