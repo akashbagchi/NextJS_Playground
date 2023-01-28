1ec2a0f762ab2383252c4e2a4f6c4c60f38043c8
Akash Bagchi <akashbagchi2710@gmail.com>
2023-01-29 01:58:50 +0530

    Flex

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -6,16 +6,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='flex-column space-y-5'>
-      <div className='bg-red-500 py-5 pl-4 m-2 mt-8 rounded-lg w-4/5 mx-auto'>
-        <h1 className='text-3xl underline mb-3 font-bold'>Title 1</h1>
-        <h2 className='text-2xl line-through'>Title 2</h2>
-        <h3 className='text-xl'>Title 3</h3>
-      </div>
-      <div className='bg-slate-500 py-5 pl-4 mb-2 rounded-lg w-4/5 mx-auto'>
-        <p className='text-base overline decoration-slate-500'>A regular paragraph</p>
-        <p className='text-sm'>A description paragraph</p>
-        <p className="note text-xs">A little note</p>
+    <main className='space-y-5'>
+      <div className="Parent">
+        <div>Header</div>
+        <div className="flex items-center justify-evenly">
+          <div className=" bg-red-400">Sidebar</div>
+          <div className=' bg-blue-300'>
+            <h1 className='text-2xl font-bold'>Main Content</h1>
+            <p>
+              Commodo elit non nulla mollit tempor esse dolore. Fugiat incididunt amet est exercitation quis Lorem deserunt. Anim sint mollit Lorem deserunt.
+            </p>
+          </div>
+          <div className='bg-red-400'>Another sidebar</div>
+        </div>
       </div>
     </main>
   )
