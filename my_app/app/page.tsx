13d2fe36de6e38206d0ed27e6eb698ca7d5a0ae2
Akash Bagchi <akashbagchi2710@gmail.com>
2023-01-29 01:19:46 +0530

    minor changes to title

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className='flex-column space-y-5'>
-      <div className='bg-red-500 py-5 pl-4 m-2 rounded-lg w-4/5 mx-auto'>
+      <div className='bg-red-500 py-5 pl-4 m-2 mt-8 rounded-lg w-4/5 mx-auto'>
         <h1 className='text-3xl underline mb-3 font-bold'>Title 1</h1>
         <h2 className='text-2xl line-through'>Title 2</h2>
         <h3 className='text-xl'>Title 3</h3>
